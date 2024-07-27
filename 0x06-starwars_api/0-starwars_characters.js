121834788ac9984cdf09a2b80dbbea2675d9c4d9
sara-mohamd <saraamohamed0038@gmail.com>
2024-07-27 21:29:01 +0300

    modification

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,38 @@
 #!/usr/bin/node
-
 const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  order(actors, 0);
-});
-const order = (actors, i) => {
-  if (i === actors.length) return;
-  request(actors[i], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    order(actors, i + 1);
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    try {
+      const charactersURL = JSON.parse(body).characters;
+      const charactersName = charactersURL.map(
+        url => new Promise((resolve, reject) => {
+          request(url, (promiseErr, __, charactersReqBody) => {
+            if (promiseErr) {
+              reject(promiseErr);
+            }
+            resolve(JSON.parse(charactersReqBody).name);
+          });
+        })
+      );
+
+      Promise.all(charactersName)
+        .then(names => console.log(names.join('\n')))
+        .catch(allErr => {
+          console.log(allErr);
+          process.exit(1);
+        });
+    } catch (jsonErr) {
+      console.log('Error parsing JSON:', jsonErr);
+      process.exit(1);
+    }
   });
-};
+} else {
+  console.log('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
