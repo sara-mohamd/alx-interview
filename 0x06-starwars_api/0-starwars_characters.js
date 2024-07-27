8c46cdcfbe46df00fb6ad58dfd7e7870807d41ae
sara-mohamd <saraamohamed0038@gmail.com>
2024-07-27 21:30:20 +0300

    modification

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,33 +6,20 @@ if (process.argv.length > 2) {
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
       console.log(err);
-      process.exit(1);
     }
-    try {
-      const charactersURL = JSON.parse(body).characters;
-      const charactersName = charactersURL.map(
-        url => new Promise((resolve, reject) => {
-          request(url, (promiseErr, __, charactersReqBody) => {
-            if (promiseErr) {
-              reject(promiseErr);
-            }
-            resolve(JSON.parse(charactersReqBody).name);
-          });
-        })
-      );
-
-      Promise.all(charactersName)
-        .then(names => console.log(names.join('\n')))
-        .catch(allErr => {
-          console.log(allErr);
-          process.exit(1);
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
         });
-    } catch (jsonErr) {
-      console.log('Error parsing JSON:', jsonErr);
-      process.exit(1);
-    }
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
-} else {
-  console.log('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
 }
