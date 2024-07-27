0a8c054db2fc2726ac744946966128452e81fb84
sara-mohamd <saraamohamed0038@gmail.com>
2024-07-27 21:22:04 +0300

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,38 @@
-#!/usr/bin/env node
-const axios = require('axios');
+#!/usr/bin/node
+const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length !== 3) {
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    try {
+      const charactersURL = JSON.parse(body).characters;
+      const charactersName = charactersURL.map(
+        url => new Promise((resolve, reject) => {
+          request(url, (promiseErr, __, charactersReqBody) => {
+            if (promiseErr) {
+              reject(promiseErr);
+            }
+            resolve(JSON.parse(charactersReqBody).name);
+          });
+        })
+      );
+
+      Promise.all(charactersName)
+        .then(names => console.log(names.join('\n')))
+        .catch(allErr => {
+          console.log(allErr);
+          process.exit(1);
+        });
+    } catch (jsonErr) {
+      console.log('Error parsing JSON:', jsonErr);
+      process.exit(1);
+    }
+  });
+} else {
   console.log('Usage: ./0-starwars_characters.js <movie_id>');
   process.exit(1);
 }
-
-const movieId = process.argv[2];
-const baseUrl = 'https://swapi.dev/api/films/';
-
-axios.get(`${baseUrl}${movieId}/`)
-  .then(response => {
-    const characters = response.data.characters;
-    const characterPromises = characters.map(url => axios.get(url));
-
-    return Promise.all(characterPromises);
-  })
-  .then(characterResponses => {
-    characterResponses.forEach(response => {
-      console.log(response.data.name);
-    });
-  })
-  .catch(error => {
-    console.error(`Error fetching data: ${error}`);
-  });
